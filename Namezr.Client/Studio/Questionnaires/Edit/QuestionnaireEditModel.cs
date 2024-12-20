dd5386a8a2e91f8879c0dba4a4397c81237ca413
xymanek <xymanek@outlook.com>
2024-12-20 19:59:39 +0200

    Basic questionnaire editor

--- Namezr.Client/Studio/Questionnaires/Edit/QuestionnaireEditModel.cs
+++ Namezr.Client/Studio/Questionnaires/Edit/QuestionnaireEditModel.cs
@@ -1,23 +1,29 @@
 ﻿namespace Namezr.Client.Studio.Questionnaires.Edit;
 
-// TODO: make these records?
-
 public class QuestionnaireEditModel
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 
-    public List<QuestionnaireFieldEditModel> Fields { get; } = new();
+    public List<QuestionnaireFieldEditModel> Fields { get; set; } = new();
+
+    public void AddBlankField()
+    {
+        Fields.Add(new QuestionnaireFieldEditModel
+        {
+            Id = Guid.NewGuid(),
+        });
+    }
 }
 
 public class QuestionnaireFieldEditModel
 {
     public required Guid Id { get; set; }
 
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 
-    public QuestionnaireFieldType Type { get; set; }
+    public QuestionnaireFieldType? Type { get; set; }
 }
 
 public enum QuestionnaireFieldType
