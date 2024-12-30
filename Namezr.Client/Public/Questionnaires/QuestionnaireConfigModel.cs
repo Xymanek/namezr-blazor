c1072553d13e190ce26b0fd8e5019ec29b6c93d3
xymanek <xymanek@outlook.com>
2024-12-30 21:48:27 +0200

    [WIP] Submission

---
+++ Namezr.Client/Public/Questionnaires/QuestionnaireConfigModel.cs
@@ -0,0 +1,25 @@
+﻿using Namezr.Client.Studio.Questionnaires.Edit;
+
+namespace Namezr.Client.Public.Questionnaires;
+
+public class QuestionnaireConfigModel
+{
+    public required string Title { get; init; }
+    public required string? Description { get; init; }
+
+    public List<QuestionnaireConfigFieldModel> Fields { get; init; } = new();
+}
+
+public class QuestionnaireConfigFieldModel
+{
+    public required Guid Id { get; init; }
+
+    public required string Title { get; init; }
+    public required string? Description { get; init; }
+    
+    public required QuestionnaireFieldType Type { get; init; }
+    
+    public required QuestionnaireTextFieldOptionsModel? TextOptions { get; init; }
+    public required QuestionnaireNumberFieldOptionsModel? NumberOptions { get; init; }
+    public required QuestionnaireFileUploadFieldOptionsModel? FileUploadOptions { get; init; }
+}
---
+++ Namezr/Features/Questionnaires/Pages/SubmissionMapper.cs
@@ -0,0 +1,32 @@
+﻿using Namezr.Client.Public.Questionnaires;
+using Namezr.Features.Questionnaires.Data;
+using Riok.Mapperly.Abstractions;
+
+namespace Namezr.Features.Questionnaires.Pages;
+
+[Mapper(UseDeepCloning = true, RequiredMappingStrategy = RequiredMappingStrategy.Target)]
+public static partial class SubmissionMapper
+{
+    [MapNestedProperties(nameof(QuestionnaireVersionEntity.Questionnaire))]
+    public static partial QuestionnaireConfigModel MapToConfigModel(this QuestionnaireVersionEntity source);
+
+    private static List<QuestionnaireConfigFieldModel> MapToConfigModel(
+        ICollection<QuestionnaireFieldConfigurationEntity> source
+    )
+    {
+        List<QuestionnaireConfigFieldModel> target = new(source.Count);
+
+        target.AddRange(
+            source
+                .OrderBy(x => x.Order)
+                .Select(MapToConfigModel)
+        );
+
+        return target;
+    }
+
+    [MapNestedProperties(nameof(QuestionnaireFieldConfigurationEntity.Field))]
+    private static partial QuestionnaireConfigFieldModel MapToConfigModel(
+        this QuestionnaireFieldConfigurationEntity source
+    );
+}
