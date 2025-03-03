f442ffedf4b42e04d2175beb9ee41065ce465ba4
xymanek <xymanek@outlook.com>
2025-03-03 16:58:08 +0200

    Show previous values when loading submission UI

---
+++ Namezr.Client/Public/Questionnaires/FieldValuesCloner.cs
@@ -0,0 +1,12 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace Namezr.Client.Public.Questionnaires;
+
+[Mapper(UseDeepCloning = true)]
+public static partial class FieldValuesCloner
+{
+    public static partial void DeepCloneInto(
+        this Dictionary<string, SubmissionValueModel> source,
+        Dictionary<string, SubmissionValueModel> target
+    );
+}
