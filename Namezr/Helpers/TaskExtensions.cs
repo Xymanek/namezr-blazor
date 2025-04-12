f93821fd8e44ee8ea8085a77bad0b6a58703382a
xymanek <xymanek@outlook.com>
2025-04-12 12:35:05 +0300

    Limit how long the login fetching can take

--- Namezr/Helpers/TaskExtensions.cs
+++ Namezr/Helpers/TaskExtensions.cs
@@ -2,6 +2,33 @@
 
 internal static class TaskExtensions
 {
-    public static async Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks) 
+    public static async Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks)
         => await Task.WhenAll(tasks);
+
+    /// <summary>
+    /// Creates a task that resolves with <see cref="OperationCanceledException"/>
+    /// when the <paramref name="cancellationToken"/> is triggered.
+    ///
+    /// Does not resolve in any other way.
+    /// </summary>
+    public static Task AsTask(this CancellationToken cancellationToken)
+    {
+        TaskCompletionSource tcs = new();
+        cancellationToken.Register(() => tcs.TrySetCanceled());
+
+        return tcs.Task;
+    }
+
+    /// <summary>
+    /// If the <paramref name="cancellationToken"/> is triggered before the task completes,
+    /// immediately resolves the returned task with <see cref="OperationCanceledException"/>.
+    /// </summary>
+    public static async Task<T> DiscardWhenCancelled<T>(this Task<T> task, CancellationToken cancellationToken)
+    {
+        await Task.WhenAny(task, cancellationToken.AsTask());
+        
+        // If the task completed, this a very cheap operation.
+        // If the cancellation token was triggered, we will not reach here.
+        return await task;
+    }
 }
