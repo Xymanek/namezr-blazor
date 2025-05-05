7456859d23821b8e56829cbc0819420be10deb63
Xymanek <xymanek@outlook.com>
2025-05-05 19:05:40 +0300

    Docs

--- Namezr/Features/Questionnaires/Data/QuestionnaireSubmissionEntity.cs
+++ Namezr/Features/Questionnaires/Data/QuestionnaireSubmissionEntity.cs
@@ -34,16 +34,26 @@ public class QuestionnaireSubmissionEntity : SelectionCandidateEntity
     public ICollection<QuestionnaireFieldValueEntity>? FieldValues { get; set; }
 }
 
+/// <remarks>
+/// Check
+/// <see cref="M:Namezr.Infrastructure.Data.Migrations.SubmissionNumber.Up(Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder)"/>
+/// for the actual SQL of the functions and triggers
+/// </remarks>
 internal class QuestionnaireSubmissionEntityConfiguration : IEntityTypeConfiguration<QuestionnaireSubmissionEntity>
 {
     public void Configure(EntityTypeBuilder<QuestionnaireSubmissionEntity> builder)
     {
         builder.Property(s => s.Number)
+            // The value will be generated only on INSERT, but using "OnAdd" will result in npgsql
+            // generating a sequence to populate - which we don't want
             .ValueGeneratedOnAddOrUpdate();
 
         builder.ToTable(table =>
         {
+            // The BEFORE INSERT trigger that achieves the above-mentioned generation logic
             table.HasTrigger("set_number");
+            
+            // Ensure that the value was successfully populated
             table.HasCheckConstraint("CK_Number_AboveZero", "\"Number\" > 0");
         });
 
--- Namezr/Features/Questionnaires/Data/SubmissionNumberSequenceEntity.cs
+++ Namezr/Features/Questionnaires/Data/SubmissionNumberSequenceEntity.cs
@@ -3,6 +3,11 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Namezr.Features.Questionnaires.Data;
 
+/// <summary>
+/// While this is exposed as an EFC entity, it's mainly used by/via
+/// <c>questionnaire_submission_get_next_number_by_version_id</c> - see
+/// <see cref="M:Namezr.Infrastructure.Data.Migrations.SubmissionNumber.Up(Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder)"/>
+/// </summary>
 [EntityTypeConfiguration(typeof(SubmissionNumberSequenceEntityConfiguration))]
 public class SubmissionNumberSequenceEntity
 {
