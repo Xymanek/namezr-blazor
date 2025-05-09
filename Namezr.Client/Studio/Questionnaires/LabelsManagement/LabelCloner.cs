5aa8efba7851f2bb6c432eb4a2e0487d5151cea4
Xymanek <xymanek@outlook.com>
2025-05-09 18:12:14 +0300

    [WIP] Label editing

---
+++ Namezr.Client/Studio/Questionnaires/LabelsManagement/LabelCloner.cs
@@ -0,0 +1,10 @@
+﻿using Namezr.Client.Shared;
+using Riok.Mapperly.Abstractions;
+
+namespace Namezr.Client.Studio.Questionnaires.LabelsManagement;
+
+[Mapper]
+public static partial class LabelCloner
+{
+    public static partial SubmissionLabelModel Clone(this SubmissionLabelModel model);
+}
