7d3c5402d9a96670b545c08296581328999a7079
xymanek <xymanek@outlook.com>
2025-05-17 14:45:17 +0300

    [WIP] Submissions table

---
+++ Namezr.Client/Studio/Questionnaires/Submissions/SubmissionFilterModel.cs
@@ -0,0 +1,20 @@
+﻿using Namezr.Client.Types;
+
+namespace Namezr.Client.Studio.Questionnaires.Submissions;
+
+public class SubmissionFilterModel
+{
+    public decimal? MinWeight { get; set; }
+    public decimal? MaxWeight { get; set; }
+
+    public List<EligibilityPlanId> IncludedPlanIds { get; set; } = [];
+    public List<EligibilityPlanId> ExcludedPlanIds { get; set; } = [];
+
+    public List<Guid> IncludedLabelIds { get; set; } = [];
+    public List<Guid> ExcludedLabelIds { get; set; } = [];
+
+    /// <summary>
+    /// Null if filter not set. Otherwise value here = value on submission
+    /// </summary>
+    public bool? MatchIsApproved { get; set; }
+}
---
+++ Namezr.Client/Studio/Questionnaires/Submissions/SubmissionTableItemModel.cs
@@ -0,0 +1,22 @@
+﻿using Namezr.Client.Public.Questionnaires;
+using Namezr.Client.Shared;
+using Namezr.Client.Types;
+
+namespace Namezr.Client.Studio.Questionnaires.Submissions;
+
+public class SubmissionTableItemModel
+{
+    public required Guid Id { get; init; }
+    public required int Number { get; init; }
+
+    public required string UserDisplayName { get; init; }
+    public required EligibilityResultModel Eligibility { get; init; }
+
+    public required DateTimeOffset InitiallySubmittedAt { get; init; }
+    public required DateTimeOffset LastUpdateAt { get; init; }
+    
+    public required bool IsApproved { get; init; }
+    
+    public required IReadOnlyList<SubmissionLabelModel> Labels { get; init; }
+    public required IReadOnlyDictionary<Guid, SubmissionValueModel> Values { get; set; }
+}
