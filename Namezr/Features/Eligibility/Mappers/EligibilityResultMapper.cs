6232a78b04abf003fa1a4255c070de5ec047874f
xymanek <xymanek@outlook.com>
2025-05-17 23:48:22 +0300

    [WIP] Submissions table 4

---
+++ Namezr/Features/Eligibility/Mappers/EligibilityResultMapper.cs
@@ -0,0 +1,11 @@
+﻿using Namezr.Client.Types;
+using Namezr.Features.Eligibility.Services;
+using Riok.Mapperly.Abstractions;
+
+namespace Namezr.Features.Eligibility.Mappers;
+
+[Mapper(UseDeepCloning = true)]
+public static partial class EligibilityResultMapper
+{
+    public static partial EligibilityResultModel ToModel(this EligibilityResult result);
+}
