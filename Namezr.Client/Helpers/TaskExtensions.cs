b54e72b2f259d62af6701f70a208e96501c2a903
Xymanek <xymanek@outlook.com>
2025-05-22 07:45:54 +0300

    [WIP] Bulk apply labels 1

---
+++ Namezr.Client/Helpers/TaskExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Namezr.Client.Helpers;
+
+public static class TaskExtensions
+{
+    public static Task WhenAll(this IEnumerable<Task> tasks)
+    {
+        return Task.WhenAll(tasks);
+    }
+}
