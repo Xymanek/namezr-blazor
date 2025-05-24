5554bfb5d7e26e902674f24e980f92b5ff505432
Xymanek <xymanek@outlook.com>
2025-05-24 08:18:14 +0300

    [WIP] Submission image grid 8

---
+++ Namezr.Client/Helpers/CollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Namezr.Client.Helpers;
+
+public static class CollectionExtensions
+{
+    public static void SetIsPresent<T>(this ISet<T> set, bool presence, T element)
+    {
+        if (presence)
+        {
+            set.Add(element);
+        }
+        else
+        {
+            set.Remove(element);
+        }
+    }
+}
