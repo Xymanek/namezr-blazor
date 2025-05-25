85384ce91cc1be602bc6d707392c2f7570506d96
xymanek <xymanek@outlook.com>
2025-05-25 15:38:34 +0300

    [WIP] Submission attributes 1

---
+++ Namezr/Features/Questionnaires/Data/SubmissionAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Namezr.Features.Questionnaires.Data;
+
+[EntityTypeConfiguration(typeof(SubmissionAttributeConfiguration))]
+public class SubmissionAttributeEntity
+{
+    public QuestionnaireSubmissionEntity Submission { get; set; } = null!;
+    public Guid SubmissionId { get; set; }
+
+    [MaxLength(KeyMaxLength)]
+    public required string Key { get; init; }
+
+    [MaxLength(ValueMaxLength)]
+    public required string Value { get; init; }
+
+    internal const int KeyMaxLength = 50;
+    internal const int ValueMaxLength = 500;
+}
+
+internal class SubmissionAttributeConfiguration : IEntityTypeConfiguration<SubmissionAttributeEntity>
+{
+    public void Configure(EntityTypeBuilder<SubmissionAttributeEntity> builder)
+    {
+        builder.HasKey(x => new { x.SubmissionId, x.Key });
+    }
+}
---
+++ Namezr/Features/Questionnaires/Endpoints/UpdateSubmissionLabelEndpoint.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using Immediate.Apis.Shared;
+using Immediate.Handlers.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Namezr.Client;
+using Namezr.Features.Questionnaires.Data;
+using Namezr.Features.Questionnaires.Services;
+using Namezr.Infrastructure.Data;
+
+namespace Namezr.Features.Questionnaires.Endpoints;
+
+[Handler]
+[Authorize]
+[MapPost(ApiEndpointPaths.SubmissionAttributeUpdate)]
+internal partial class UpdateSubmissionLabelEndpoint
+{
+    public class Request
+    {
+        public required Guid SubmissionId { get; init; }
+
+        public required string Key { get; init; }
+        public required string Value { get; init; }
+
+        [RegisterSingleton(typeof(IValidator<Request>))]
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+                RuleFor(r => r.Key)
+                    .MaximumLength(SubmissionAttributeEntity.KeyMaxLength);
+
+                RuleFor(r => r.Value)
+                    .MaximumLength(SubmissionAttributeEntity.ValueMaxLength);
+            }
+        }
+    }
+
+    private static async ValueTask<IResult> HandleAsync(
+        Request request,
+        IHttpContextAccessor httpContextAccessor,
+        IDbContextFactory<ApplicationDbContext> dbContextFactory,
+        ISubmissionAuditService auditService,
+        CancellationToken ct
+    )
+    {
+        // TODO: fetch submission
+        // TODO: validate access
+    }
+}
