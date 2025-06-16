e0ec38d6e90f2699102ae61838799c83597614d9
Xymanek <xymanek@outlook.com>
2025-06-16 17:49:41 +0300

    [WIP] Notifications 8

---
+++ Namezr/Features/Notifications/Contracts/INotificationDiscordRender.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+
+namespace Namezr.Features.Notifications.Contracts;
+
+internal interface INotificationDiscordRender
+{
+    ValueTask<RenderedDiscordNotification?> RenderDirectMessageIfSupportedAsync(Notification notification);
+}
+
+internal record RenderedDiscordNotification
+{
+    public string? Text { get; init; }
+    public required Embed RichEmbed { get; init; }
+    public required Embed[] Embeds { get; init; }
+}
--- Namezr/Features/Notifications/Services/NotificationSenderDiscord.cs
+++ Namezr/Features/Notifications/Services/NotificationSenderDiscord.cs
@@ -1,4 +1,10 @@
-﻿using Namezr.Features.Notifications.Contracts;
+﻿using AspNet.Security.OAuth.Discord;
+using Discord;
+using Discord.Rest;
+using Microsoft.EntityFrameworkCore;
+using Namezr.Features.Notifications.Contracts;
+using Namezr.Infrastructure.Data;
+using Namezr.Infrastructure.Discord;
 
 namespace Namezr.Features.Notifications.Services;
 
@@ -10,12 +16,60 @@ internal interface INotificationSenderDiscord
     Task<bool> SendIfSupported(Notification notification);
 }
 
+[AutoConstructor]
 [RegisterSingleton]
-internal class NotificationSenderDiscord : INotificationSenderDiscord
+internal partial class NotificationSenderDiscord : INotificationSenderDiscord
 {
+    private readonly IDbContextFactory<ApplicationDbContext> _dbContextFactory;
+    private readonly IEnumerable<INotificationDiscordRender> _renderers;
+    private readonly IDiscordApiProvider _apiProvider;
+
     public async Task<bool> SendIfSupported(Notification notification)
     {
-        // throw new NotImplementedException();
-        return false; // TODO: implement
+        ulong[] discordIds = await GetUserDiscordIds(notification);
+        if (discordIds.Length == 0) return false;
+
+        RenderedDiscordNotification? rendered = await MaybeRender(notification);
+        if (rendered == null) return false;
+
+        await using DiscordRestClient client = await _apiProvider.GetDiscordApiForApp();
+
+        foreach (ulong discordId in discordIds)
+        {
+            // TODO: error handling per each
+            RestUser user = await client.GetUserAsync(discordId);
+            await user.SendMessageAsync(rendered.Text, embed: rendered.RichEmbed, embeds: rendered.Embeds);
+        }
+
+        // TODO: return false if none of the IDs succeeded.
+        return true;
+    }
+
+    private async Task<ulong[]> GetUserDiscordIds(Notification notification)
+    {
+        if (notification.Recipient.UserId == null) return [];
+
+        await using ApplicationDbContext dbContext = await _dbContextFactory.CreateDbContextAsync();
+
+        string[] userDiscordIds = await dbContext.UserLogins
+            .Where(login =>
+                login.UserId == notification.Recipient.UserId &&
+                login.LoginProvider == DiscordAuthenticationDefaults.AuthenticationScheme
+            )
+            .Select(login => login.ProviderKey)
+            .Distinct()
+            .ToArrayAsync();
+
+        return userDiscordIds.Select(ulong.Parse).ToArray();
+    }
+
+    private async Task<RenderedDiscordNotification?> MaybeRender(Notification notification)
+    {
+        foreach (INotificationDiscordRender renderer in _renderers)
+        {
+            return await renderer.RenderDirectMessageIfSupportedAsync(notification);
+        }
+
+        return null;
     }
 }
