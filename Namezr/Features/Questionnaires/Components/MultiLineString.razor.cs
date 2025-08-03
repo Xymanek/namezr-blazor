f69cf453615e59ebd45913477aa9a75a0c58d5d9
Xymanek <xymanek@outlook.com>
2025-08-03 11:37:02 +0300

    Better comment (e.g. bio) display

---
+++ Namezr/Features/Questionnaires/Components/MultiLineString.razor.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Namezr.Features.Questionnaires.Components;
+
+public partial class MultiLineString
+{
+    [GeneratedRegex(@"(\r\n|\r|\n)")]
+    private partial Regex GetNewLineRegex();
+}
