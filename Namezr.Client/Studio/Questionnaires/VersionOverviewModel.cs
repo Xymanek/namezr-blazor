f2ca6df1a6f8714d875f03fd9db826bcdc5e5c13
Xymanek <xymanek@outlook.com>
2025-08-31 08:54:16 +0300

    Remove the timestamp from QuestionnaireFieldsOverview

--- Namezr.Client/Studio/Questionnaires/VersionOverviewModel.cs
+++ Namezr.Client/Studio/Questionnaires/VersionOverviewModel.cs
@@ -6,7 +6,6 @@ public class VersionOverviewModel
 {
     public required Guid Id { get; init; }
     public required int Number { get; init; }
-    public required DateTimeOffset CreatedAt { get; init; }
 
     public required QuestionnaireFieldEditModel[] Fields { get; init; }
 }
